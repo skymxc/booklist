5f81c2a5c67ed57ae23f4e68ecd9f164034d354d
skymxc <skymxc@foxmail.com>
2020-03-30 22:59:25 +0800

    首页界面&逻辑更改
    增加了悬浮的按钮，增加了书单缓存。

--- miniprogram/js/User.js
+++ miniprogram/js/User.js
@@ -1,9 +1,13 @@
+ /**
+  * 从本地缓存获取 openid
+  */
  function getOpenidFromStroage() {
    return new Promise(function(resolve,reject){
      var res = wx.getStorageInfoSync();
      if (res.keys.indexOf('openid') != -1) {
        var resposne = wx.getStorageSync("openid");
-       resolve(resposne.data)
+       console.log("getOpenidFromStroage ->"+resposne)
+       resolve(resposne)
 
      } else {
        reject({
@@ -27,8 +31,10 @@
    return new Promise(function(resolve,reject){
      getOpenidFromStroage().then(res=>{
         //本地就有
+        console.log(res)
         resolve(res)
      }).catch(error=>{
+       console.log(error)
        //本地没有，从服务器获取吧
        getOpenidFromServer().then(res=>{
          //存储在本地
--- miniprogram/pages/booklist/booklist.js
+++ miniprogram/pages/booklist/booklist.js
@@ -1,5 +1,7 @@
 // miniprogram/pages/booklist/booklist.js
-const { $Message } = require('../../dist/base/index');
+const {
+  $Message
+} = require('../../dist/base/index');
 const Users = require('../../js/User.js');
 
 const app = getApp();
@@ -11,202 +13,238 @@ Page({
    */
   data: {
     bookList: [],
-    enableEmpty: false,
-    modalTitle:'',
-    modalVisible:false,
-    modalContent:'请选择操作',
-    modalActions:[{
-      name:'删除',
-      color:'red'
-    },{
-      name:'编辑',
-      color:'green'
-    },{
-      name:'取消'
+    modalTitle: '',
+    modalVisible: false,
+    modalContent: '请选择操作',
+    modalActions: [{
+      name: '删除',
+      color: 'red'
+    }, {
+      name: '编辑',
+      color: 'green'
+    }, {
+      name: '取消'
     }],
-    modalBook:{},
-    modalBookIndex:-1
+    modalBook: {},
+    modalBookIndex: -1,
+    enableEmpty: false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function(options) {
+  onLoad: function (options) {
+
+    wx.showLoading({
+      title: '加载中',
+    })
     var openid = Users.getOpenid();
-     openid.then(res=> console.log('openid->',res))
-     .catch(error=>console.error(error));
-    /**
-     * 检查权限 
-     */
-    // wx.showLoading({
-    //   title: '正在检查授权信息',
-    //   mask: true
-    // });
-    if (app.globalData.logged) {
-      this.loadBookList(true);
-    } else {
-
-      var that = this;
-      app.showLoadingMask('正在检查授权');
-      app.handlePermissionAndGetUserInfo().then(res => {
-        if (res.auth) {
-
-          app.showLoadingMask('登陆中');
-          app.login().then(res => {
-            wx.hideLoading();
-            //获取数据 booklist
-            that.loadBookList(true);
-          }).catch(error => {
-
-            app.showErrNoCancel("登陆失败", error.errMsg);
-          });
+    openid.then(res => {
+        console.log('openid->', res)
+        if (res) {
+          app.globalData.openid = res
+          //从本地缓存中去数据
+            this.loadBookListFromStorage()
         } else {
-          // 去授权页
-
-          wx.reLaunch({
-            url: '../authorize/authorize',
-            success: function() {
-              wx.hideLoading();
-            },
-            fail: function(error) {
-
-              app.showErrNoCancel("跳转授权页失败", error.errMsg);
-
-            }
-          });
+          wx.showModal({
+            cancelColor: 'cancelColor',
+            content: 'openid 错误'
+          })
         }
-      }).catch(error => {
-
-        wx.reLaunch({
-          url: '../authorize/authorize',
-          success: function () {
-            wx.hideLoading();
-          },
-          fail: function (error) {
-
-            app.showErrNoCancel("跳转授权页失败", error.errMsg);
+      })
+      .catch(error => {
+        console.error("获取openID 出现错误-" + error)
+        wx.showModal({
+          showCancel: false,
+          content: error,
+          success: function (response) {
 
           }
-        });
-
+        })
       });
-    }
   },
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function() {
-    
-    this.loadBookList(true);
+  onPullDownRefresh: function () {
+    wx.hideLoading()
+    this.loadBookList();
   },
-
   /**
-   * 页面上拉触底事件的处理函数
+   * 从缓存里加载
+   * 加载成功后后直接显示，然后从后台刷新出最新数据
+   * 加载失败或者没有数据时出发刷新，从后台获取
    */
-  onReachBottom: function() {
-    // this.loadBookList(false);
-  },
-  loadBookList: function(refresh) {
-    
-      app.showLoading("加载中");
-    
+  loadBookListFromStorage: function () {
     var that = this;
+    wx.getStorage({
+      key: 'bookList',
+    }).then(res => {
+      console.log("storage data-", res)
+      if (res) {
+        that.data.bookList = res;
+          wx.hideLoading()
+        that.setData({
+          bookList: that.data.bookList,
+          enableEmpty: that.data.bookList.length == 0
+        });
+        //从后台刷新数据
+        this.loadBookList()
+      }else{
+        wx.startPullDownRefresh()
+      }
+    }).catch(error => {
+      console.error("从缓存里取书单错误，", error)
+        wx.startPullDownRefresh()
+    })
+  },
+  /**
+   * 从后台加载数据并缓存到本地
+   */
+  loadBookList: function () {
+    var that = this;
+    
     db.collection('book_list').where({
         _openid: app.globalData.openid
       }).get()
       .then(res => {
-        wx.hideLoading();
+        console.log("从后台获取到的书单",res)
         wx.stopPullDownRefresh();
 
-        if (refresh) {
-          that.data.bookList = res.data;
-          that.data.pageIndex = res.data.length;
-        } else {
-          that.data.bookList = that.data.bookList.concat(res.data);
-          that.data.pageIndex = that.data.pageIndex + res.data.length;
-        }
+        that.data.bookList = res.data;
+
         that.setData({
           bookList: that.data.bookList,
-         
           enableEmpty: that.data.bookList.length == 0
         });
+
+        this.saveBookListToStorage()
+
       }).catch(error => {
-        app.showErr("数据异常", error.errMsg)
+        wx.stopPullDownRefresh();
+        console.error("书单加载错误->",error)
+        wx.showModal({
+          showCancel: false,
+          content: error,
+          title: "提示",
+          success: (res) => {
+            if (res.confirm) {
+              wx.startPullDownRefresh()
+            }
+          }
+        })
+        // app.showErr("数据异常", error.errMsg)
       });
   },
-  tapCreateBooklist: function() {
-    if(this.data.bookList.length>=20){
+
+  /**
+   * 保存当前显示的书单到本地缓存
+   */
+  saveBookListToStorage:function(){
+   var data = this.data.bookList;
+    wx.setStorage({
+      data: data,
+      key: 'bookList',
+      success:function(){
+        console.log("缓存到本地成功-",data)
+      },
+      fail:function(err){
+        console.error("缓存到本地失败",err)
+      }
+    })
+  },
+  /**
+   * 创建书单被点击
+   */
+  tapCreateBooklist: function () {
+    if (this.data.bookList.length >= 20) {
       app.showErrNoCancel('提示', '最多创建20个书单，目前：' + this.data.bookList.length)
       return;
     }
-    app.showLoadingMask('请稍后');
+    wx.showLoading({
+      title: '请稍后',
+    })
     wx.navigateTo({
       url: '../createBooklist/createBooklist',
-      success: function() {
+      success: function () {
         wx.hideLoading();
       },
-      fail: function(error) {
-
-        app.showErrNoCancel('跳转异常', error.errMsg);
+      fail: function (error) {
+        console.log("跳转到创建异常，",error)
+        wx.showModal({
+          showCancel:false,
+          content:'跳转异常'
+        })
+        
       }
     })
   },
-  tapBook: function(event) {
+
+  /**
+   * 书单被点击
+   * @param {*} event 
+   */
+  tapBook: function (event) {
     var book = event.currentTarget.dataset.book;
     app.showLoadingMask('请稍后');
     wx.navigateTo({
-      url: '../listDetail/listDetail?_id='+book._id,
-      success:function(){
+      url: '../listDetail/listDetail?_id=' + book._id,
+      success: function () {
         wx.hideLoading();
       }
     })
   },
-  longTapBook: function (event){
+  /**
+   * 书单被长按
+   * @param {*} event 
+   */
+  longTapBook: function (event) {
     var book = event.currentTarget.dataset.book;
     var index = event.currentTarget.dataset.index;
     this.setData({
-      modalBook:book,
-      modalTitle:book.name,
-      modalVisible:true,
-      modalBookIndex:index
+      modalBook: book,
+      modalTitle: book.name,
+      modalVisible: true,
+      modalBookIndex: index
     })
   },
-  handleModalClick: function ({ detail }){
+  handleModalClick: function ({detail
+  }) {
     const index = detail.index;
-    var that =this;
+    var that = this;
     if (index === 0) {
-      
+
       app.showLoadingMask('删除中');
-      var that =this;
+      var that = this;
       db.collection('book_list')
-      .doc(this.data.modalBook._id).remove()
-      .then(res=>this.handleDelBooklist(res))
-      .then(res=>{
-        console.log('删除所属书籍-',res)
-        wx.hideLoading();
-        if(res){
-          wx.showToast({
-            title: '删除成功',
-          })
-          that.data.bookList.splice(that.data.modalBookIndex, 1);
-          that.setData({
-            bookList: that.data.bookList
-          })
-        }
-      })
-      .catch(error=>{
-        app.showErrNoCancel('删除失败',error.errMsg);
-      })
+        .doc(this.data.modalBook._id).remove()
+        .then(res => this.handleDelBooklist(res))
+        .then(res => {
+          console.log('删除所属书籍-', res)
+          wx.hideLoading();
+          if (res) {
+            wx.showToast({
+              title: '删除成功',
+            })
+            that.data.bookList.splice(that.data.modalBookIndex, 1);
+            that.setData({
+              bookList: that.data.bookList
+            })
+          }
+        })
+        .catch(error => {
+          app.showErrNoCancel('删除失败', error.errMsg);
+        })
     } else if (index === 1) {
       app.showLoadingMask('请稍后');
       wx.setStorage({
         key: 'editbooklist',
         data: that.data.modalBook,
-        success:function(){
+        success: function () {
           app.navigateTo('../editBooklist/editBooklist')
         },
-        fail:function(err){
-          app.showErrNoCancel('编辑失败',err.errMsg);
+        fail: function (err) {
+          app.showErrNoCancel('编辑失败', err.errMsg);
         }
       })
     }
@@ -215,19 +253,19 @@ Page({
       modalVisible: false
     });
   },
-  handleDelBooklist(res){
+  handleDelBooklist(res) {
     if (res.stats.removed == 1) {
       //删除书单里的书籍
-      var that =this;
-       return wx.cloud.callFunction({
-        name:'delBookBylistId',
-        data:{
-          listid:that.data.modalBook._id
+      var that = this;
+      return wx.cloud.callFunction({
+        name: 'delBookBylistId',
+        data: {
+          listid: that.data.modalBook._id
         }
       });
-      
+
     } else {
-     
+
       app.showToast('删除失败')
     }
   }
